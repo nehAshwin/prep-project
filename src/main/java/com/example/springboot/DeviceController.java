1f061df78ec4689a7e1d6e85a880e79b0bd5cecd
Neha Ashwin <107515350+nehAshwin@users.noreply.github.com>
2026-05-25 21:52:50 -0700

    fixed error: missing import

--- src/main/java/com/example/springboot/DeviceController.java
+++ src/main/java/com/example/springboot/DeviceController.java
@@ -5,6 +5,7 @@ package com.example.springboot;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
